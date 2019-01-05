52316115dc4090b39981e42a10e5095ecc6c8e4a
AlexQuidditch <alexquidditch@gmail.com>
2019-01-06 02:32:20 +0300

    - откат конфига - удалён dotenv

--- config/index.js
+++ config/index.js
@@ -1,3 +1,21 @@
-const { parsed } = require('dotenv').config();
+const {
+  BOT_TOKEN,
+  DB_HOST,
+  MONGODB_URI,
+  MONGOLAB_AMBER_URI,
+  MONGOLAB_COPPER_URI,
+  PROD_MONGODB,
+  PORT,
+} = process.env;
 
-module.exports = parsed;
+const config = {
+  BOT_TOKEN,
+  DB_HOST,
+  MONGODB_URI,
+  MONGOLAB_AMBER_URI,
+  MONGOLAB_COPPER_URI,
+  PROD_MONGODB,
+  PORT,
+};
+
+module.exports = config;
