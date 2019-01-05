1a9460929f3596d138168ca41a2b3cb6a01534ab
AlexQuidditch <alexquidditch@gmail.com>
2019-01-06 02:06:01 +0300

    - переписано получение конфига

--- config/index.js
+++ config/index.js
@@ -1,10 +1,3 @@
-require('dotenv').config();
+const { parsed } = require('dotenv').config();
 
-const { BOT_TOKEN, DB_HOST } = process.env;
-
-const config = {
-  BOT_TOKEN,
-  DB_HOST,
-};
-
-module.exports = config;
+module.exports = parsed;
