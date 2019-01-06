a6ea931d52b2e9acd7f885a6a0c0ef0e4a8c0446
AlexQuidditch <alexquidditch@gmail.com>
2019-01-06 04:42:43 +0300

    - доработка модели

--- src/model/user/Schema.js
+++ src/model/user/Schema.js
@@ -2,37 +2,43 @@ const { Schema } = require('mongoose');
 const BaseSchemaAbstract = require('../base/Schema');
 
 const ModelSchema = {
-  is_bot: {
+	user_id: {
+		type: String,
+		required: true,
+	},
+	bot_id: {
     type: String,
-    required: true,
   },
-  first_name: {
-    type: String,
-    required: true,
-  },
-  last_name: {
-    type: String,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  language_code: {
-    type: String,
-    required: true,
-  },
-  chat_id: {
-    type: String,
-    required: true,
-  },
-  last_activity: {
+	chat_id: {
+		type: String,
+		required: true,
+	},
+	is_bot: {
+		type: String,
+		required: true,
+	},
+	first_name: {
+		type: String,
+		required: true,
+	},
+	last_name: {
+		type: String,
+		required: true,
+	},
+	username: {
+		type: String,
+		required: true,
+	},
+	language_code: {
+		type: String,
+		required: true,
+	},
+	last_activity: {
     type: Date,
   },
 	active: {
 		type: Boolean,
-		required: true,
-		default: false,
+		default: true,
 	},
 };
 
